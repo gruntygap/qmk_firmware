60b4d0094105b4d60ebcba85f489f0c7aa099ddc
gruntygap <gruntygap@gmail.com>
2023-12-18 17:27:24 -0600

    add gamer tings

--- keyboards/ferris/keymaps/brogrammer/config.h
+++ keyboards/ferris/keymaps/brogrammer/config.h
@@ -16,6 +16,7 @@
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 100
 
 // Pick good defaults for enabling homerow modifiers
+// https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md
 #define TAPPING_TERM 200
+#define QUICK_TAP_TERM 120
 #define PERMISSIVE_HOLD
-#define QUICK_TAP_TERM 0
